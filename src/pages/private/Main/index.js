747631f49c09c2d5139ff30ed17fd62ff621b3eb
Jean-Donald Roselin <donniecent@live.fr>
2022-02-06 01:29:01 +0100

    Le placeholder de la page explorer est terminé

--- src/pages/private/Main/index.js
+++ src/pages/private/Main/index.js
@@ -7,14 +7,44 @@ import api from '../../../services/api';
 
 import { Container, ContainerScrollView } from './styles';
 
+const MEDIA_PLACEHOLDER = {
+  id: 0,
+  previewImage: null,
+  title: '',
+  type: "audio",
+  section: 'predication',
+  durationInSecond: 0,
+  positionInSecond: 0,
+  releaseDateTimestamp: 101010000,
+  releaseDate: "05-12-2001",
+  playUrl: "",
+  downloadUrl: "",
+  isFavorite: false,
+  description: "",
+  author: { fullName : 'chargement' },
+  featuring: [],
+  album: null,
+  category: []
+};
+const EXPLORER_PLACEHOLDER_ROW = { title: 'hhh', medias: [MEDIA_PLACEHOLDER, MEDIA_PLACEHOLDER, MEDIA_PLACEHOLDER, MEDIA_PLACEHOLDER, MEDIA_PLACEHOLDER] };
+
 export default function Main() {
   const [mainList, setMainList] = useState([]);
+  const [mainListPlaceholder] = useState([
+    EXPLORER_PLACEHOLDER_ROW,
+    EXPLORER_PLACEHOLDER_ROW,
+    EXPLORER_PLACEHOLDER_ROW,
+    EXPLORER_PLACEHOLDER_ROW
+  ]);
+
   const { setCurrentPlaylist } = useContext(PlayerContext);
 
   useEffect(() => {
     function loadMainList() {
       api.post('/media/explorer').then((response) => {
-        setMainList(response.data.data)
+        setTimeout(function() {
+          setMainList(response.data.data)
+        }, 500)
       }).catch(e => console.error(e));
     }
     loadMainList();
@@ -27,9 +57,13 @@ export default function Main() {
   return (
     <Container>
       <ContainerScrollView>
-        {mainList &&
+        {mainList.length > 0 &&
           mainList.map((item, index) => (
-            <PodcastsSlide key={index} list={item} onItemPress={onItemPress} />
+            <PodcastsSlide key={index} list={item} isPlaceholder={false} onItemPress={onItemPress} />
+          ))}
+        {mainList.length === 0 &&
+          mainListPlaceholder.map((item, index) => (
+             <PodcastsSlide key={index} list={item} isPlaceholder={true} onItemPress={onItemPress} />
           ))}
       </ContainerScrollView>
     </Container>
--- src/pages/private/Search/index.js
+++ src/pages/private/Search/index.js
@@ -25,7 +25,7 @@ import { PlayerContext } from '~/context/player.context';
 
 export default function Search() {
   const [categories, setCategories] = useState([]);
-  const [categoriesPlaceholder, setCategoriesPlaceholder] = useState([{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14}]);
+  const [categoriesPlaceholder] = useState([{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11},{id:12},{id:13},{id:14}]);
   const [isSearching, setIsSearching] = useState(false);
   const [searchAudios, setSearchAudios] = useState([]);
   const { setCurrentPlaylist } = useContext(PlayerContext);
