8e88e80b18d191af59988c47b327a200e71d3de3
Jean-Donald Roselin <donniecent@live.fr>
2022-02-27 09:35:50 +0100

    Integration d'un plugin de placeholder adapté à expo et implémentation sur l'accueil + la page de recherche

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -8,24 +8,37 @@ import {
   Description,
 } from './styles';
 import { View } from "react-native";
-import { Title } from "../PodcastsSlide/styles";
-import SkeletonContent from 'react-native-skeleton-content';
+import SkeletonLoader from "expo-skeleton-loader";
 
 export default function Card({ item, isPlaceholder, onPress }) {
   return (
     <Container onPress={() => onPress(item)}>
-      <PodImage source={{ uri: item.previewImage }} style={{ backgroundColor: 'rgba(255,255,255,0.1)' }} />
+      {
+        isPlaceholder ?
+          <SkeletonLoader>
+            <SkeletonLoader.Container style={[{ flex: 1, flexDirection: "row" }]} >
+              <SkeletonLoader.Item style={{  width: 150,  height: 150 }}/>
+            </SkeletonLoader.Container>
+          </SkeletonLoader>
+          :
+          <PodImage source={{ uri: item.previewImage }} style={{ backgroundColor: 'rgba(255,255,255,0.1)' }} />
+      }
       <ContainerSubtitle>
         {
-          <SkeletonContent
-            containerStyle={{ flex: 1 }}
-            isLoading={isPlaceholder}
-          >
+          isPlaceholder ?
+            <SkeletonLoader>
+                <SkeletonLoader.Container style={[{ flex: 1, flexDirection: "row" }]} >
+                  <SkeletonLoader.Item style={{  width: 150,  height: 10 }}/>
+                </SkeletonLoader.Container>
+                <SkeletonLoader.Container style={[{ flex: 1, flexDirection: "row" }]} >
+                  <SkeletonLoader.Item style={{  width: 150,  height: 10 }}/>
+                </SkeletonLoader.Container>
+            </SkeletonLoader>
+            :
             <>
               <Subtitle>{item.title}</Subtitle>
               <Description>{item.author.fullName}</Description>
             </>
-          </SkeletonContent>
         }
       </ContainerSubtitle>
     </Container>
--- src/components/PodcastsSlide/index.js
+++ src/components/PodcastsSlide/index.js
@@ -3,16 +3,29 @@ import { View } from 'react-native';
 
 import Card from '../Card';
 import { Container, Title, PodcastList } from './styles';
+import SkeletonLoader from "expo-skeleton-loader";
+
 
 export default function PodcastsSlide({ list, isPlaceholder, onItemPress }) {
   return (
     <Container>
-      <SkeletonContent
-            containerStyle={{ flex: 1 }}
-            isLoading={isPlaceholder}
-          >
-        <Title>{list.title}</Title>
-      </SkeletonContent>
+      {
+        isPlaceholder ?
+          <SkeletonLoader>
+            <SkeletonLoader.Container
+              style={[{ flex: 1, flexDirection: "row" }]}
+            >
+              <SkeletonLoader.Item
+                style={{
+                  width: 150,
+                  height: 13,
+                }}
+              />
+            </SkeletonLoader.Container>
+          </SkeletonLoader>
+          :
+          <Title>{list.title}</Title>
+      }
       <PodcastList>
         {list.medias &&
           list.medias.map((item, index) => <Card key={index} item={item} isPlaceholder={isPlaceholder} onPress={onItemPress} />)}
